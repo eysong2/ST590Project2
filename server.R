728b4812cff0bebf026d79fb8fadd0a17bbfa4fe
eysong2 <44578492+eysong2@users.noreply.github.com>
2018-11-30 11:59:04 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -4,6 +4,13 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 library(data.table)
+library(stargazer)
+library(rpart)
+library(haven)
+library(knitr)
+library(tree)
+library(class)
+library(dataPreparation)
 
 myData <- read.csv(file="https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv")
 
@@ -14,6 +21,8 @@ shinyServer(function(input, output, session) {
     newData <- myData %>% filter(Ethnicity == input$ethnicity)
   })
   
+# Descriptive statistics ---------------------------------------------------------------------------
+  
   #create sub-title 
   output$densitytitle <- renderUI({
     h4(paste("Density of Packs Per Day by Ethnicity"))
@@ -22,7 +31,7 @@ shinyServer(function(input, output, session) {
   #draw density plot
   plotInput <- reactive({
     ggplot() + 
-      geom_density(aes(x=PacksPerDay, fill = Ethnicity), data=myData, alpha = 0.5) 
+      geom_density(aes(x=PacksPerDay, fill = Ethnicity), data=myData, alpha = 0.5) + xlab("Number of packs smoked per day")
   })
   
   output$density <- renderPlot({
@@ -40,7 +49,22 @@ shinyServer(function(input, output, session) {
       dev.off()
     }
    )
-
+  
+ #download data for the plot
+  densitydata <- reactive({
+    densitydata <- myData %>% 
+      dplyr::select(SEQN, Ethnicity, PacksPerDay)
+  })
+  
+  # Download Data for this tab
+  output$densitydata_csv <- downloadHandler(
+    filename = "densitydata.csv",
+    content = function(file) {
+      write.csv(densitydata(), file)
+    },
+    contentType = "text/csv"
+  )
+  
  #add special symbol using withMathJax
   output$formula <- renderUI({
     withMathJax(
@@ -56,9 +80,9 @@ shinyServer(function(input, output, session) {
   #create histogram by Ethnicity
   histInput <- reactive({
    if (input$var1=='IncomeGroup'){
-    ggplot(myData, aes(x = IncomeGroup, fill = Ethnicity)) + geom_bar()
-   } else if (input$var1=='Education'){
-    ggplot(myData, aes(x = Education, fill = Ethnicity)) + geom_bar()
+    ggplot(myData, aes(x = IncomeGroup, fill = Ethnicity)) + geom_bar(aes(y = ((..count..)/sum(..count..))*100)) + labs(x = "Income Group", y = "Percent")
+     } else if (input$var1=='Education'){
+    ggplot(myData, aes(x = Education, fill = Ethnicity)) + geom_bar(aes(y = ((..count..)/sum(..count..))*100)) + labs( y = "Percent") 
    } 
   })
   
@@ -78,6 +102,8 @@ shinyServer(function(input, output, session) {
     }
   )
   
+# Scatter plot --------------------------------------------------------------------------------------
+  
   #create renderUI for sub-title with user interfaces 
   output$scatertitle <- renderUI({
     h3(paste("Scatter Plot of Age & Packs per Day for", input$ethnicity, sep =" "))
@@ -88,7 +114,7 @@ shinyServer(function(input, output, session) {
     #get filtered data
     newData <- df1()
     #create plot
-    g <- ggplot(newData, aes(x = Age, y = PacksPerDay))
+    g <- ggplot(newData, aes(x = Age, y = PacksPerDay)) + ylab("Number of packs smoked per day")
     
     if(input$income){
       g + geom_point(aes(col = IncomeGroup))
@@ -144,10 +170,127 @@ shinyServer(function(input, output, session) {
     }  
   })
   
-  #create data table by Ethnicity    
+  
+#create data table by Ethnicity--------------------------------------------------------------------------    
   output$table <- renderDataTable(
     newData <- df1()
   )
+  
+  
+
+ #Supervised learning model 1 - Regression modeling-----------------------------------------------------
+
+  modelData <- subset(myData) %>% select(Ethnicity, Education, IncomeGroup, PacksPerDay)
+  
+  linearInput <- reactive({
+    if (input$var1=='IncomeGroup'){
+      ggplot(modelData, aes(x = IncomeGroup, y = PacksPerDay, color = Ethnicity)) + geom_point() + geom_smooth(method = "lm") + ylab("Number of packs smoked per day")
+    } else if (input$var1=='Education'){
+      ggplot(modelData, aes(x = Education, y = PacksPerDay, color = Ethnicity)) + geom_point() + geom_smooth(method = "lm") + ylab("Number of packs smoked per day")
+    } 
+  })
+  
+  # plot model
+  output$lmPlot <- renderPlot({
+    linearInput()
+  })
+  
+  linearMod <- reactive({
+    if (input$var1=='IncomeGroup'){
+      lm(PacksPerDay ~ IncomeGroup, data=myData)
+     } else if (input$var1=='Education'){
+      lm(PacksPerDay ~ Education, data=myData) 
+    } 
+  })
+    
+  output$linearmodSum <- renderPrint({
+    summary(linearMod())
+  })
+
+ 
+ #Supervised learning model 2 - Tree-Based modeling----------------------------------------------------------------
+  
+  treeData <- subset(myData) %>% select(Ethnicity, Age, Education, IncomeGroup, PacksPerDay)
+  
+  # grow tree   
+  fitInput <- reactive({
+    if (input$cp=='0'){
+      rpart(PacksPerDay ~ ., data = treeData, method = "class", control =rpart.control(minsplit =1,minbucket=1, cp=0))
+    } else if (input$cp=='0.0025'){
+      rpart(PacksPerDay ~ ., data = treeData, method = "class", control =rpart.control(minsplit =1,minbucket=1, cp=0.0025))
+    } 
+  })
+  
+  # plot tree 
+  output$treePlot <- renderPlot({
+    plot(fitInput(), compress=TRUE)
+    title(main="Classification Tree ")
+  })
+   
+  output$treeSum <- renderPrint({
+    summary(fitInput())
+  }) 
+  
+  
+#Unsupervised learning model: PCA--------------------------------------------------------------------------
+  
+  df <- subset(myData) %>% select(INDHHINC, INDFMPIR, INDFMINC, DMDHHSIZ, Age, Education, PacksPerDay)
+  pcaData <- na.omit(df)
+  
+  #standardize the variables 
+  scales <- build_scales(dataSet = pcaData, cols = c("INDHHINC", "INDFMPIR", "INDFMINC", "DMDHHSIZ", "Age", "Education", "PacksPerDay"), verbose = TRUE)
+  pcaDataScale <- fastScale(dataSet = pcaData, scales = scales, verbose = TRUE)
+  
+  PCs <- prcomp(select(pcaDataScale, INDHHINC, INDFMPIR, INDFMINC, DMDHHSIZ, Age, Education, PacksPerDay), center = TRUE, scale = TRUE)
+  
+  output$pcaSum <- renderPrint({
+    summary(PCs)
+  })
+
+  #compute standard deviation of each principal component
+  std_dev <- PCs$sdev
+  
+  #compute variance
+  pr_var <- std_dev^2
+  
+  #check variance of components
+  pr_var[1:7]
+  
+  #proportion of variance explained
+  prop_varex <- pr_var/sum(pr_var)
+  
+  # select number of PCs   
+  pcaInput <- reactive({
+    if (input$pca=='5'){
+      prop_varex[1:5]
+    } else if (input$pca=='6'){
+      prop_varex[1:6]
+    } else if (input$pca=='7'){
+      prop_varex[1:7]
+    }  
+    })
+  
+  output$varexPlot <- renderPlot({
+    plot(pcaInput(), xlab = "Principal Component",
+         ylab = "Variance",
+         type = "b")
+  })
+  
+  bipInput <- reactive({
+    if (input$pca=='5'){
+      PCs <- prcomp(select(pcaDataScale, INDHHINC, INDFMPIR, INDFMINC, DMDHHSIZ, Age), center = TRUE, scale = TRUE)
+    } else if (input$pca=='6'){
+      PCs <- prcomp(select(pcaDataScale, INDHHINC, INDFMPIR, INDFMINC, DMDHHSIZ, Age, Education), center = TRUE, scale = TRUE)
+    } else if (input$pca=='7'){
+      PCs <- prcomp(select(pcaDataScale, INDHHINC, INDFMPIR, INDFMINC, DMDHHSIZ, Age, Education, PacksPerDay), center = TRUE, scale = TRUE)
+    }  
+  })
+  
+  output$pcaPlot <- renderPlot({
+    biplot(bipInput(), xlabs = rep(".", nrow(pcaDataScale)), cex = 1.2)
+  })
+  
+# Infomation page -------------------------------------------------------------------------------------------------
   
   #create renderUI for sub-title
   output$infotitle <- renderUI({
@@ -170,6 +313,35 @@ shinyServer(function(input, output, session) {
     paste("3. Table Tab: You can check data table by ethnicity and download the data tables.")
   })
   
+  output$tab4 <- renderText({
+    paste("4. Regression Tab: You can examine the relationship between Number of packs smoked per day with variable of your interest.")
+  })
+  
+  output$tab5 <- renderText({
+    paste("5. Tree-BasedModels Tab: You can create a model that predicts the value of a target variable based on several input variables.")
+  })
+  
+  output$tab6 <- renderText({
+    paste("6. Principal Components Analysis Tab: You can reduce a large set of variables to a small set that still contains most of the information in the large dataset.")
+  })  
+   
+  output$package <- renderText({
+    paste("**Package needed:   library(shiny)
+  library(tidyverse)
+  library(dplyr)
+  library(readr)
+  library(ggplot2)
+  library(data.table)
+  library(stargazer)
+  library(rpart)
+  library(haven)
+  library(knitr)
+  library(tree)
+  library(class)
+  library(dataPreparation)")
+  })    
+  
+
   #create renderUI for sub-title 
   output$datatitle <- renderUI({
     h3(paste("2. About the Data:"))
@@ -177,9 +349,56 @@ shinyServer(function(input, output, session) {
   
   #create info text
   output$datainfo <- renderText({
-    paste("Data is collected from adults in the US regarding on demographic characteristics and smoking behaviors. Total number of sample size is 443. Data is available at https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv and also can download from here.")
+    paste("This is survey data collected by the US National Center for Health Statistics (NCHS) which has conducted a series of health and nutrition surveys since the early 1960's. Since 1999 approximately
+          5,000 individuals of all ages are interviewed in their homes every year and complete the health examination component of the survey. The health examination is conducted in a mobile examination
+          centre (MEC). Total number of sample size is 443. Data is available at https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv and also can download from here.")
   })
   
+  #create renderUI for sub-title 
+  output$vartitle <- renderUI({
+    h4(paste("Data Dictionary"))
+  })
+  
+  output$var2info <- renderText({
+    paste("Gender - Gender")
+  })
+  
+  output$var3info <- renderText({
+    paste("Age - Age")
+  })
+  
+  output$var4info <- renderText({
+    paste("IncomeGroup - Income (Less Than 20K vs. More Than 20K)")
+  })
+  
+  output$var4binfo <- renderText({
+    paste("INDHHINC - Estimated total household income")
+  })
+ 
+  output$var4cinfo <- renderText({
+    paste("INDFMPIR - Ratio of family income to poverty")
+  }) 
+
+  output$var4dinfo <- renderText({
+    paste("INDFMINC - Annual Family Income")
+  })   
+
+  output$var5info <- renderText({
+    paste("Ethnicity - Ethnicity (MexicanAmerican & Hispanic, Non-Hispanic Black, Non-Hispanic Caucasian)")
+  })
+  
+  output$var6info <- renderText({
+    paste("Education - Education (1:8th grade, 2:9-11th grade, 3: High School, 4:Some college, 5: College graduate)")
+  })
+
+  output$var7info <- renderText({
+    paste("PacksPerDay - Number of packs smoked per day")
+  })
+  
+  output$var8info <- renderText({
+    paste("DMDHHSIZ - Number of people in the participant's household")
+  })  
+
   #create renderUI for sub-title 
   output$smoketitle <- renderUI({
     h3(paste("3. Smoking Facts:"))
@@ -191,7 +410,14 @@ shinyServer(function(input, output, session) {
           nearly half a million Americans die prematurely of smoking or exposure to secondhand smoke. Another 16 million live with a serious illness caused by smoking. Each year, the United States spends nearly $170 billion on medical care to treat smoking-related disease in adults.")
   })
   
-  output$info2 <- renderText({
-    paste("Reference: CDC(2018). Smoking & Tobacco Use. Derived from https://www.cdc.gov/tobacco/data_statistics/fact_sheets/index.htm?s_cid=osh-stu-home-spotlight-001 on October 27, 2018.")
+  output$reftitle <- renderUI({
+    h4(paste("Reference:"))
+  })
+  
+  output$ref1 <- renderText({
+    paste("1. CDC(2018). Smoking & Tobacco Use. Derived from https://www.cdc.gov/tobacco/data_statistics/fact_sheets/index.htm?s_cid=osh-stu-home-spotlight-001 on October 27, 2018.")
+  })
+  output$ref2 <- renderText({
+    paste("2. Hlavac, Marek (2018). stargazer: Well-Formatted Regression and Summary Statistics Tables. R package version 5.2.2. https://CRAN.R-project.org/package=stargazer")
   })
 })
--- ui.R
+++ ui.R
@@ -4,6 +4,13 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 library(data.table)
+library(stargazer)
+library(rpart)
+library(haven)
+library(knitr)
+library(tree)
+library(class)
+library(dataPreparation)
 
 myData <- read.csv(file="https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv")
 
@@ -18,7 +25,7 @@ shinyUI(fluidPage(
       br(),
       br(),
       br(),
-      selectizeInput("var1", "Descriptive statistics: Select Variable", selected='IncomeGroup', 
+      selectizeInput("var1", "Descriptive Statistics & Regression Models: Select Variable", selected='Education', 
                        choices = c("Income"="IncomeGroup", "Education"="Education")), 
       br(),
       br(),
@@ -30,6 +37,12 @@ shinyUI(fluidPage(
         condition="input.income ==true",
         checkboxInput("summary", h4("Show summary statistics!", style = "color:green;"))
       ),
+      br(),
+      selectizeInput("cp", "Tree-Base Models: Select Complexity Parameter of the tree (CP)", selected='0', 
+                     choices = c("0", "0.0025")),
+      br(),
+      selectizeInput("pca", "Principal Components Analysis: Select Number of PCs", selected='5', 
+                     choices = c("5","6","7")),
       br(),
       helpText(   a("Click Here to Download Data", href="https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv", target="_blank"))
     ),
@@ -45,13 +58,32 @@ shinyUI(fluidPage(
                    textOutput("tab1"),
                    textOutput("tab2"),
                    textOutput("tab3"),
+                   textOutput("tab4"),
+                   textOutput("tab5"),
+                   textOutput("tab6"),
+                   br(),
+                   textOutput("package"), 
                    uiOutput("datatitle"),
                    textOutput("datainfo"),
+                   uiOutput("vartitle"),
+                   textOutput("var2info"),
+                   textOutput("var3info"),
+                   textOutput("var4info"),
+                   textOutput("var4binfo"),
+                   textOutput("var4cinfo"),
+                   textOutput("var4dinfo"),
+                   textOutput("var5info"),
+                   textOutput("var6info"),
+                   textOutput("var7info"),
+                   textOutput("var8info"),
                    helpText(   a("Click Here to Download Data", href="https://www4.stat.ncsu.edu/~post/558/datasets/smokeData.csv", target="_blank")),
                    uiOutput("smoketitle"),
                    textOutput("info1"),
                    helpText(   a("Click Here to get more information on Smoking & Tobacco Use in the U.S.", href="https://www.cdc.gov/tobacco/index.htm", target="_blank")),
-                   textOutput("info2")
+                   br(),
+                   uiOutput("reftitle"),
+                   textOutput("ref1"),
+                   textOutput("ref2")
                  )
                 ),
         tabPanel("Descriptive", 
@@ -60,12 +92,13 @@ shinyUI(fluidPage(
                    # create sub-title
                    uiOutput("densitytitle"),
                    plotOutput("density"),
+                   downloadButton('densitydata_csv', "Download the data for the density graph"),
                    withMathJax(),
                    uiOutput("formula"),
                    downloadButton('downloadDensity', "Download the density plot"),
                    uiOutput("histtitle"),
                    plotOutput("hist"),
-                   downloadButton('downloadHistogram', "Download the histogram")                 
+                   downloadButton('downloadHistogram', "Download the histogram")     
                   )
                 ),
         tabPanel("Plot", 
@@ -78,8 +111,31 @@ shinyUI(fluidPage(
                    verbatimTextOutput("clickinfo")
                   )
                 ),        
-        tabPanel("Table", dataTableOutput('table'))
-      )
+        tabPanel("Table", dataTableOutput('table')
+                 ),
+        tabPanel("Regression Models", 
+                 fluidRow(
+                   helpText(   a("Regression Model Plots and Model Fit Results Summary")),
+                   tabPanel("Linear plot", p(), plotOutput("lmPlot")),
+                   verbatimTextOutput("linearmodSum")
+                 )
+                ),
+        tabPanel("Tree-Based Models", 
+                 fluidRow(
+                   helpText(   a("Classification Trees Plots and Model Fit Results Summary")),
+                   plotOutput("treePlot"),
+                   verbatimTextOutput("treeSum")
+                  )
+                ),
+        tabPanel("Principal Components Analysis", 
+                 fluidRow(
+                   helpText(   a("Principal Components Analysis (PCA)")),
+                   plotOutput("varexPlot"),
+                   plotOutput("pcaPlot"),
+                   verbatimTextOutput("pcaSum")
+                 )
+                )
+        )
     )
   )
 ))
